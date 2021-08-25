b455f16b7875fac37d415770177a05c8f83cd7f9
“heeyoungster” <yll111@naver.com>
2021-08-25 23:47:14 +0900

    first

---
+++ js/script.js
@@ -0,0 +1,39 @@
+var $lotto = [];
+
+for(i=0; i<6; i++){
+    var $num = Math.ceil(Math.random() * 45);
+    if($lotto.indexOf($num) == -1){
+        $lotto.push("<span>" + $num + "</span>");
+    }
+    else{
+        continue
+    }
+}
+console.log($lotto);
+var $lotto2 = $lotto.join();
+console.log($lotto2);
+var lottoNum = $lotto2.replace(/,/g,"");
+console.log(lottoNum);
+document.write("<div class='lotto'>" + lottoNum + "</div>");
+/*
+var $color = [
+    ["background: #0ed;color: #333;"],
+    ["background: #e6a;color: #fff;"],
+    ["background: #c3e; color: #fff;"],
+    ["background: #df3;color: #333;"],
+    ["background: #37f;color: #fff;"],
+    ["background: #7e6;color: #333;"],
+]
+var $c_frame = document.querySelector("body");
+var $c_box = "";
+console.log($color.length);
+
+for(j=0; j<$color.length; j++){
+    $c_box += `
+    <span style="`+$color[j]+`"></span>
+    `;
+}
+console.log($c_box);
+$c_frame.innerHTML = $c_box;
+*/
+
