52156b4c722ab0000a4379f4f55673751ae4a892
“heeyoungster” <yll111@naver.com>
2021-09-07 18:34:33 +0900

    second

--- js/script.js
+++ js/script.js
@@ -1,39 +1,25 @@
-var $lotto = [];
-
-for(i=0; i<6; i++){
-    var $num = Math.ceil(Math.random() * 45);
-    if($lotto.indexOf($num) == -1){
-        $lotto.push("<span>" + $num + "</span>");
+var lotto_cover = document.querySelector("body .case");
+var lotto_ball = "";
+var lotto = [];
+var num;
+while(lotto.length < 6){
+    num = Math.floor(Math.random() * 46) + 1; 
+    console.log(num);
+    if(lotto.indexOf(num) == -1){
+        lotto.push(num);
     }
-    else{
-        continue;
-    }
+    console.log(lotto);
 }
-console.log($lotto);
-var $lotto2 = $lotto.join();
-console.log($lotto2);
-var lottoNum = $lotto2.replace(/,/g,"");
-console.log(lottoNum);
-document.write("<div class='lotto'>" + lottoNum + "</div><p>새로운 번호를 보려면 새로고침 하세요</p>");
-/*
-var $color = [
-    ["background: #0ed;color: #333;"],
-    ["background: #e6a;color: #fff;"],
-    ["background: #c3e; color: #fff;"],
-    ["background: #df3;color: #333;"],
-    ["background: #37f;color: #fff;"],
-    ["background: #7e6;color: #333;"],
-]
-var $c_frame = document.querySelector("body");
-var $c_box = "";
-console.log($color.length);
+var lotto_arr = lotto.sort(function(a, b){
+    return a - b;
+});
+console.log(lotto_arr);
+console.log(lotto_arr.length);
 
-for(j=0; j<$color.length; j++){
-    $c_box += `
-    <span style="`+$color[j]+`"></span>
-    `;
+for(i=0; i <lotto_arr.length; i++){
+    lotto_ball += `
+    <span>`+lotto_arr[i]+`</span>
+    `
 }
-console.log($c_box);
-$c_frame.innerHTML = $c_box;
-*/
-
+console.log(lotto_cover);
+lotto_cover.innerHTML = lotto_ball;
